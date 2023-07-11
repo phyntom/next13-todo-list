73ef6e9474eecd741acea255d92c77d2da63f5ac
AIMABLE RUHUMURIZA <aruhumuriza@microsoft.com>
2023-07-12 02:57:12 +0300

    adding error handle for client side and ecosystem config for azure app service

--- next.config.js
+++ next.config.js
@@ -3,13 +3,11 @@ const nextConfig = {
 	experimental: {
 		serverActions: true,
 	},
-    eslint: {
-    // Warning: This allows production builds to successfully complete even if
-    // your project has ESLint errors.
-    ignoreDuringBuilds: true,
-  },
-  distDir: 'build',
-  output:"standalone"
+	eslint: {
+		// Warning: This allows production builds to successfully complete even if
+		// your project has ESLint errors.
+		ignoreDuringBuilds: true,
+	},
 };
 
 module.exports = nextConfig;
